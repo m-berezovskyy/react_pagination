aa97fa631c7415c0a6be96fafd70ad55f584da0d
Marian <mberez999@gmail.com>
2026-07-09 02:48:36 +0300

    add solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,92 @@
-export const Pagination = () => {};
+import React from 'react';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+
+  const pages = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+
+  const handlePageClick = (page: number, e: React.MouseEvent) => {
+    e.preventDefault();
+    if (page !== currentPage) {
+      onPageChange(page);
+    }
+  };
+
+  if (totalPages <= 1) {
+    return null;
+  }
+
+  return (
+    <ul className="pagination" data-cy="pagination">
+      <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#/"
+          aria-disabled={currentPage === 1}
+          onClick={e => {
+            if (currentPage > 1) {
+              handlePageClick(currentPage - 1, e);
+            } else {
+              e.preventDefault();
+            }
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {pages.map(page => (
+        <li
+          key={page}
+          className={`page-item ${page === currentPage ? 'active' : ''}`}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href="#/"
+            onClick={e => handlePageClick(page, e)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#/"
+          aria-disabled={currentPage === totalPages}
+          onClick={e => {
+            if (currentPage < totalPages) {
+              handlePageClick(currentPage + 1, e);
+            } else {
+              e.preventDefault();
+            }
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
